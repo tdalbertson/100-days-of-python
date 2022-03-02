12800d963234718121050bb4a5f1f0c02690b7cf
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2022-03-02 01:10:11 -0800

    Create Day 1 Project.py
    Band name generator for Day 1 project.

---
+++ Day 1 Project.py
@@ -0,0 +1,12 @@
+"""
+100 Days of Python Day 1 Project
+Band Name Generator
+"""
+
+print("Welcome to the Band Name Generator!")
+
+city = input("What city did you grow up in?\n")
+
+pet_name = input("What is the name of one of your pets?\n")
+
+print(f"Your band name is {city} {pet_name}")
