defb2754c66c983fc39b396ca41df28a9dc6a26c
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2022-03-04 12:12:42 -0800

    Added functions
    Practiced creating functions to handle some of the program operations.

--- Day 2 Project.py
+++ Day 2 Project.py
@@ -12,6 +12,11 @@ def tip_switcher(tip):
     
     return switcher.get(tip, "default")
 
+def calc_total(total_bill, tip):
+    total_with_tip = (total_bill * tip) + total_bill
+    
+    return total_with_tip
+
 print("Welcome to the Tip Calculator!")
 
 bill_total = float(input("What was the total bill? "))
@@ -22,7 +27,7 @@ desired_tip = int(input("What percentage tip would you like to give? 10, 12, or 
 
 tip_percentage = tip_switcher(desired_tip)
 
-total_w_tip = (bill_total * tip_percentage) + bill_total
+total_w_tip = calc_total(bill_total, tip_percentage)
 split_total = total_w_tip / number_to_split
 
 print(f"Each person should pay: {split_total}")
